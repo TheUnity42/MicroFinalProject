45fab196226285d391e407e420efdd01e2c627ff
Talon <jtalonh@gmail.com>
2021-12-08 17:22:29 -0500

    file options

--- record.py
+++ record.py
@@ -1,38 +1,83 @@
 import pyaudio
 import wave
+import sys, getopt
 
-form_1 = pyaudio.paInt32
-chans = 1
-sample_rate = 48000
-chunk = 4096
-record_secs = 5
-dev_index = 1
-wav_output_filename = 'test1.wav'
+# dict config: {
+# form: pyaudio.paInt32
+# chans: 
+# sample_rate:
+# chunk_size: 
+# record_secs: 
+# dev_index: 
+# 
+# }
+help_str = "record.py - .wav file recorder\nUsage:\n\t-h\t\thelp\n\t-o\t<file>\toutput file name\n\t[-t]\t<sec>\tduration of recording (seconds) [default: 1]\n\t[-c]\t<num>\tNumber of channels [default: 1]"
+options_dict = {
+    'format': pyaudio.paInt32,
+    'chans': 1,
+    'sample_rate': 48000,
+    'chunk_size': 4096,
+    'record_secs': 1,
+    'dev_index': 1,
+    'filename': ""
+}
 
-audio = pyaudio.PyAudio()
 
-# create pyaudio stream object
-stream = audio.open(format=form_1, rate=sample_rate, channels=chans, input_device_index=dev_index, input=True, frames_per_buffer=chunk)
 
-print("Recording...")
-frames = []
+def record(opt):
+    audio = pyaudio.PyAudio()
 
-for i in range(0, (sample_rate//chunk)*record_secs):
-    data = stream.read(chunk)
-    
-    frames.append(data)
-    
-print("Finished Recording.")
+    # create pyaudio stream object
+    stream = audio.open(format=opt['format'], rate=opt['sample_rate'], channels=opt['chans'], input_device_index=opt['dev_index'], input=True, frames_per_buffer=opt['chunk_size'])
 
-# close resources
-stream.stop_stream()
-stream.close()
-audio.terminate()
+    print("Recording...")
+    frames = []
 
-# save wav file
-wavefile = wave.open(wav_output_filename, 'wb')
-wavefile.setnchannels(chans)
-wavefile.setsampwidth(audio.get_sample_size(form_1))
-wavefile.setframerate(sample_rate)
-wavefile.writeframes(b''.join(frames))
-wavefile.close()
+    for i in range(0, (opt['sample_rate']//opt['chunk_size'])*opt['record_secs']):
+        data = stream.read(opt['chunk_size'])        
+        frames.append(data)
+        
+    print("Finished Recording.")
+
+    # close resources
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # save wav file
+    wavefile = wave.open(opt['filename'], 'wb')
+    wavefile.setnchannels(opt['chans'])
+    wavefile.setsampwidth(audio.get_sample_size(opt['format']))
+    wavefile.setframerate(opt['sample_rate'])
+    wavefile.writeframes(b''.join(frames))
+    wavefile.close()
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "ho:t:c:")
+    except getopt.GetoptError:
+        print(help_str)
+        exit(1)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print(help_str)
+            exit(0)
+        elif opt == '-o':
+            options_dict['filename'] = arg
+        elif opt == '-t':
+            options_dict['record_secs'] = int(arg)
+        elif opt == '-c':
+            options_dict['chans'] = arg
+
+    if options_dict['filename'] == "":
+        print("Must specify filename.")
+        print(help_str)
+        exit(1)
+
+    print(f"Preparing to record {options_dict['record_secs']} seconds to {options_dict['filename']}...")
+
+    record(options_dict)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
