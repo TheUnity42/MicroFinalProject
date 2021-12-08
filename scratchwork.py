100c98f1370c0727fba8b5c30cf87b4b742ad5a4
Talon <jtalonh@gmail.com>
2021-12-08 17:47:21 -0500

    spectrogram!

--- scratchwork.py
+++ scratchwork.py
@@ -10,7 +10,7 @@ def wavHeader(wavefile: wave.Wave_read) -> dict:
             "Framerate": wavefile.getframerate(),
             "Num Frames": wavefile.getnframes(),}
 
-
+EPSILON = 1e-12
 
 def main():
 
@@ -25,6 +25,17 @@ def main():
     buffer_ft = np.fft.fft(buffer)
     x_ft = np.linspace(0, wavh['Framerate']/2, wavh['Num Frames']//2)
 
+    print("Computing Spectogram...")
+    N = 256
+    buffer_s = []
+    for k in np.arange(0, wavh['Num Frames'], N):
+        x = np.fft.fftshift(np.fft.fft(buffer[k:k+N], N))[N//2:N]
+        Pxx = 10*np.log10(np.real(x*np.conj(x))+EPSILON)
+        buffer_s.append(Pxx)
+
+    buffer_s = np.array(buffer_s)
+
+
     print("Plotting...")
     fig, ax = plt.subplots(2, 2, figsize=(10, 10))
 
@@ -33,6 +44,9 @@ def main():
 
     ax[0,1].plot(x_ft, 2.0/wavh['Num Frames'] * np.abs(buffer_ft[:wavh['Num Frames']//2]))
     ax[0,1].set_title('FFT')
+
+    ax[1,0].specgram(buffer_s[:,0])
+    ax[1,0].set_title('Spectrogram')
     
     plt.savefig('plot.png')
     
