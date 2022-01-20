da3fe0f0a5f25c731215d82659e7cbcea8d03b9e
TheUnity42 <jtalonh@gmail.com>
2022-01-20 07:21:49 -0500

    Reworked UI

--- ElectronGUI/src/App.js
+++ ElectronGUI/src/App.js
@@ -1,11 +1,41 @@
+import { useState } from "react";
 import SlideButton from "./components/SlideButton";
 
 function App() {
+  let config = useState({
+    fade: -1,
+    delay: -1,
+    reverb: -1,
+  });
+
+  function fadeCallback(value) {
+    config.setState({
+      fade: value,
+    });
+  }
+
+  function delayCallback(value) {
+    config.setState({
+      delay: value,
+    });
+  }
+
+  function reverbcallback(value) {
+    config.setState({
+      reverb: value,
+    });
+  }
+  
   return (
-    <div className="flex flex-col w-screen h-screen bg-gray-800">
-      <SlideButton text="Fade" />
-      <SlideButton text="Delay" />
-      <SlideButton text="Reverb" />
+    <div className="flex flex-row w-screen h-screen bg-gray-800 divide-gray-900 divide-x">
+      <div className="relative flex flex-col mx-1">
+        <SlideButton text="Fade" callback={fadeCallback} />
+        <SlideButton text="Delay" callback={delayCallback} />
+        <SlideButton text="Reverb" callback={reverbcallback} />
+      </div>
+      <div className="relative flex flex-col m-1">
+        <p>Fade: {config.fade}</p>
+      </div>
     </div>
   );
 }
--- ElectronGUI/src/components/SlideButton.js
+++ ElectronGUI/src/components/SlideButton.js
@@ -1,33 +1,19 @@
 import React from "react";
 
 function SlideButtonRange(props) {
-  const {
-    min,
-    max,
-    value,
-    onChange,
-  } = props;
-
-  let printValue = value;
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    printValue = e.target.value;
-  }
-
-
+  const { min, max, value, handleChange } = props;
 
   return (
-    <div className="relative flex flex-row w-36 h-6">
+    <div className="relative flex flex-row w-36 h-6 mx-2 items-center">
       <input
         type="range"
         min={min}
         max={max}
-        value={value}
-        onChange={onChange}
-        className="relative m-2"
+        defaultValue={value}
+        onChange={handleChange}
+        className="relative m-1 w-28 bg-blue-600"
       />
-      <p>{value}</p>
+      <p className="relative m-1 text-green-400">{value}</p>
     </div>
   );
 }
@@ -45,28 +31,36 @@ class SlideButton extends React.Component {
     this.setState({
       active: !this.state.active,
     });
+    this.props.callback(this.state.active ? this.state.value : -1);
   };
 
-  onChange = (e) => {
+  handleChange = (e) => {
     this.setState({
       value: e.target.value,
     });
+    this.props.callback(e.target.value);
   };
 
   render() {
-
-    let slider = '';
+    let slider = "";
     if (this.state.active) {
-      slider = <SlideButtonRange min={0} max={10} value={this.state.value} onChange={this}/>;
+      slider = (
+        <SlideButtonRange
+          min={0}
+          max={10}
+          value={this.state.value}
+          handleChange={this.handleChange}
+        />
+      );
     }
 
     return (
-      <div className="relative flex flex-col">
+      <div className="relative flex flex-col items-left">
         <button
           onClick={this.onClick}
           className={
             "relative text-black w-36 h-12 m-2 justify-center text-center align-middle rounded-xl shadow-xl " +
-            "hover:border-2 hover:translate-x-4 transition-all ease-linear " +
+            "hover:border-2 hover:rounded-none transition-all ease-linear " +
             (this.state.active
               ? "bg-green-400 hover:border-blue-600"
               : "bg-blue-600 hover:border-green-400")
